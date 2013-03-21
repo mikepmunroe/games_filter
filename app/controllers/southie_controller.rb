96f1874ad7cd0d8fed6a1d0a9725b973d6c64608
Dave Kroondyk <dave.kroondyk@thomsonreuters.com>
2013-03-21 10:33:39 -0400

    a little less duplication for queries.

--- app/controllers/southie_controller.rb
+++ app/controllers/southie_controller.rb
@@ -1,12 +1,14 @@
 class SouthieController < ApplicationController
   def index
-    @athletes = Athlete.where(:affiliate => 'southie').order("total asc")
-    @individual_men = Athlete.where(:affiliate => 'southie').where('age < 40').where('gender == "Male"').order('total asc')
-    @masters_40_44_men = Athlete.where(:affiliate => 'southie').where('age > 39 and age < 45').where('gender == "Male"').order('total asc')
-    @masters_50_54_men = Athlete.where(:affiliate => 'southie').where('age > 49 and age < 55').where('gender == "Male"').order('total asc')
-    @masters_55_59_men = Athlete.where(:affiliate => 'southie').where('age > 54 and age < 60').where('gender == "Male"').order('total asc')
-    @masters_60_men = Athlete.where(:affiliate => 'southie').where('age > 60').where('gender == "Male"').order('total asc')
-    @individual_women = Athlete.where(:affiliate => 'southie').where('age < 40').where('gender == "Female"').order('total asc')
+    athletes           = Athlete.where(:affiliate => 'southie').order('total asc')
+    male_athletes      = athletes.where("gender == 'Male'")
+    female_athletes    = athletes.where("gender == 'Female'")
+    @individual_men    = male_athletes.where('age < 40')
+    @masters_40_44_men = male_athletes.where('age > 39 and age < 45')
+    @masters_50_54_men = male_athletes.where('age > 49 and age < 55')
+    @masters_55_59_men = male_athletes.where('age > 54 and age < 60')
+    @masters_60_men    = male_athletes.where('age > 60')
+    @individual_women  = female_athletes.where('age < 40')
   end
 end
 
